cbb99c4109464eb813ee42e84351c72d23d49642
nikitc <nikitc6@rambler.ru>
2015-10-03 21:32:03 +0500

    Update

--- roman-time.js
+++ roman-time.js
@@ -1,9 +1,9 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-
-if ((hours > 23) || (minutes > 59))
+if (hours > 23 || minutes > 59) {
 	process.stdout.write('Время указано неверно.');
+}
 else {
 	var result = convertToRoman(hours) + ':' + convertToRoman(minutes);
 	process.stdout.write(result + '\n');
@@ -11,8 +11,10 @@ else {
 }
 
 function convertToRoman(value){
-	if (value == 0)
+	if (value == 0){
 		return '--'
+	}
+
 	var romanNumerals = {
 		50 : 'L',
 		40 : 'XL',
@@ -23,94 +25,98 @@ function convertToRoman(value){
 		1 : 'I'
 	}
 
-	var arrayNumbers = [50, 40, 10, 9, 5, 4, 1];
+	var arrayNumbers = Object.keys(romanNumerals).reverse();
 	var result = '';
 	var numberCount = 0;
 
-	while (value > 0) 
-	{
-		if (value >= arrayNumbers[numberCount])
-		{
+	while (value > 0) {
+		if (value >= arrayNumbers[numberCount]) {
 			value -= arrayNumbers[numberCount];
 			result += romanNumerals[arrayNumbers[numberCount]];
 		}
-		else
-			if (numberCount != arrayNumbers.length)
+		else {
+			if (numberCount != arrayNumbers.length) {
 				numberCount++;
+			}
+		}
 	}
 
 	return result;
 }
 
 function PrintASCII(result) {
-	var I_ASCII = [	' (_)(_)(_) ',
-					'    (_)    ',
-					'    (_)    ',
-					'    (_)    ',
-					'    (_)    ',
-					'    (_)    ',
-					' (_)(_)(_) '
-				]
-	var V_ASCII = [	' (_)           (_) ',
-					'  (_)         (_)  ',
-					'   (_)       (_)   ',
-					'    (_)     (_)    ',
-					'     (_)   (_)     ',
-					'      (_)_(_)      ',
-					'        (_)        '
+	var dict_ASCII = {
+					I : 
+					[	' (_)(_)(_) ',
+						'    (_)    ',
+						'    (_)    ',
+						'    (_)    ',
+						'    (_)    ',
+						'    (_)    ',
+						' (_)(_)(_) '
+					]
+						,
 
-				]
+					V : 
+					[	' (_)           (_) ',
+						'  (_)         (_)  ',
+						'   (_)       (_)   ',
+						'    (_)     (_)    ',
+						'     (_)   (_)     ',
+						'      (_)_(_)      ',
+						'        (_)        '
+					]
+						,
 
-	var X_ASCII = [	' (_)         (_) ',
-					'   (_)     (_)   ',
-					'     (_) (_)     ',
-					'       (_)       ',
-					'     (_) (_)     ',
-					'   (_)     (_)   ',
-					' (_)         (_) '
-				  
-				]
+					X : 
+					[	' (_)         (_) ',
+						'   (_)     (_)   ',
+						'     (_) (_)     ',
+						'       (_)       ',
+						'     (_) (_)     ',
+						'   (_)     (_)   ',
+						' (_)         (_) '
+					]
+						,
 
-	var L_ASCII = [  ' (_)             ',
-					' (_)             ',
-					' (_)             ',
-					' (_)             ',
-					' (_)             ',
-					' (_)             ',
-					' (_)(_)(_)(_)(_) '
-				  
-				]
-	var ASCII_2 = [	'        ',
-					'  _  _  ',
-					' (_)(_) ',
-					' (_)(_) ',
-					'  -  -  ',
-					' (_)(_) ',
-					' (_)(_) '
-				]
+					L :
+					[	' (_)             ',
+						' (_)             ',
+						' (_)             ',
+						' (_)             ',
+						' (_)             ',
+						' (_)             ',
+						' (_)(_)(_)(_)(_) '
+					]
+						,
 
-	var ASCII_0 = [	'        ',
-					'        ',
-					'        ',
-					' (_)(_) ',
-					'        ',
-					'        ',
-					'        '
-				]
+					':' : 
+					[	'        ',
+						'  _  _  ',
+						' (_)(_) ',
+						' (_)(_) ',
+						'  -  -  ',
+						' (_)(_) ',
+						' (_)(_) '
+					]
+						,
 
-	var dict_ASCII = {'I' : I_ASCII,
-					 'V' : V_ASCII,
-					 'X' : X_ASCII,
-					 'L' : L_ASCII,
-					 ':' : ASCII_2,
-					 '-' : ASCII_0
+					'-' :
+					[	'        ',
+						'        ',
+						'        ',
+						' (_)(_) ',
+						'        ',
+						'        ',
+						'        '
+					] 
 					}
 
-	for (var numberStr = 0; numberStr < 7; numberStr++)
-	{
+	for (var numberStr = 0; numberStr < 7; numberStr++) {
 		str = '';
-		for (var index = 0; index < result.length; index++)
+		for (var index = 0; index < result.length; index++) {
 			str += dict_ASCII[result[index]][numberStr];
+		}
 		process.stdout.write(str + '\n');
 	}
 
