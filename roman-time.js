20d40ccb0c506f7b1c972d4842c8a8634361cab0
nikitc <nikitc6@rambler.ru>
2015-10-03 22:03:49 +0500

    Update 2

--- roman-time.js
+++ roman-time.js
@@ -11,113 +11,123 @@ else {
 }
 
 function convertToRoman(value){
-	if (value == 0){
-		return '--'
-	}
+    if (value == 0){
+        return '--'
+    }
 
-	var romanNumerals = {
-		50 : 'L',
-		40 : 'XL',
-		10 : 'X',
-		9 : 'IX',
-		5 : 'V',
-		4 : 'IV',
-		1 : 'I'
-	}
+    var romanNumerals = {
+        50 : 'L',
+        40 : 'XL',
+        10 : 'X',
+        9 : 'IX',
+        5 : 'V',
+        4 : 'IV',
+        1 : 'I'
+    }
 
-	var arrayNumbers = Object.keys(romanNumerals).reverse();
-	var result = '';
-	var numberCount = 0;
+    var arrayNumbers = Object.keys(romanNumerals).sort(compareNumeric);
+    var result = '';
+    var numberCount = 0;
 
-	while (value > 0) {
-		if (value >= arrayNumbers[numberCount]) {
-			value -= arrayNumbers[numberCount];
-			result += romanNumerals[arrayNumbers[numberCount]];
-		}
-		else {
-			if (numberCount != arrayNumbers.length) {
-				numberCount++;
-			}
-		}
-	}
+    while (value > 0) {
+        if (value >= arrayNumbers[numberCount]) {
+            value -= arrayNumbers[numberCount];
+            result += romanNumerals[arrayNumbers[numberCount]];
+        }
+        else {
+            if (numberCount != arrayNumbers.length) {
+                numberCount++;
+            }
+        }
+    }
 
-	return result;
+    return result;
+}
+
+function compareNumeric(a, b) {
+    if (a < b) {
+        return 1;
+    }
+    if (a > b) {
+        return -1;
+    }
 }
 
 function PrintASCII(result) {
-	var dict_ASCII = {
-					I : 
-					[	' (_)(_)(_) ',
-						'    (_)    ',
-						'    (_)    ',
-						'    (_)    ',
-						'    (_)    ',
-						'    (_)    ',
-						' (_)(_)(_) '
-					]
-						,
+    var dict_ASCII = {
+                    I : 
+                    [	' (_)(_)(_) ',
+                        '    (_)    ',
+                        '    (_)    ',
+                        '    (_)    ',
+                        '    (_)    ',
+                        '    (_)    ',
+                        ' (_)(_)(_) '
+                    ]
+                        ,
 
-					V : 
-					[	' (_)           (_) ',
-						'  (_)         (_)  ',
-						'   (_)       (_)   ',
-						'    (_)     (_)    ',
-						'     (_)   (_)     ',
-						'      (_)_(_)      ',
-						'        (_)        '
-					]
-						,
+                    V : 
+                    [	' (_)           (_) ',
+                        '  (_)         (_)  ',
+                        '   (_)       (_)   ',
+                        '    (_)     (_)    ',
+                        '     (_)   (_)     ',
+                        '      (_)_(_)      ',
+                        '        (_)        '
+                    ]
+                        ,
 
-					X : 
-					[	' (_)         (_) ',
-						'   (_)     (_)   ',
-						'     (_) (_)     ',
-						'       (_)       ',
-						'     (_) (_)     ',
-						'   (_)     (_)   ',
-						' (_)         (_) '
-					]
-						,
+                    X : 
+                    [	' (_)         (_) ',
+                        '   (_)     (_)   ',
+                        '     (_) (_)     ',
+                        '       (_)       ',
+                        '     (_) (_)     ',
+                        '   (_)     (_)   ',
+                        ' (_)         (_) '
+                    ]
 
-					L :
-					[	' (_)             ',
-						' (_)             ',
-						' (_)             ',
-						' (_)             ',
-						' (_)             ',
-						' (_)             ',
-						' (_)(_)(_)(_)(_) '
-					]
-						,
+                        ,
 
-					':' : 
-					[	'        ',
-						'  _  _  ',
-						' (_)(_) ',
-						' (_)(_) ',
-						'  -  -  ',
-						' (_)(_) ',
-						' (_)(_) '
-					]
-						,
+                    L :
+                    [	' (_)             ',
+                        ' (_)             ',
+                        ' (_)             ',
+                        ' (_)             ',
+                        ' (_)             ',
+                        ' (_)             ',
+                        ' (_)(_)(_)(_)(_) '
+                    ]
+                        ,
 
-					'-' :
-					[	'        ',
-						'        ',
-						'        ',
-						' (_)(_) ',
-						'        ',
-						'        ',
-						'        '
-					] 
-					}
+                    ':' : 
+                    [	'        ',
+                        '  _  _  ',
+                        ' (_)(_) ',
+                        ' (_)(_) ',
+                        '  -  -  ',
+                        ' (_)(_) ',
+                        ' (_)(_) '
+                    ]
+                        ,
 
-	for (var numberStr = 0; numberStr < 7; numberStr++) {
-		str = '';
-		for (var index = 0; index < result.length; index++) {
-			str += dict_ASCII[result[index]][numberStr];
-		}
-		process.stdout.write(str + '\n');
-	}
+                    '-' :
+                    [	'        ',
+                        '        ',
+                        '        ',
+                        ' (_)(_) ',
+                        '        ',
+                        '        ',
+                        '        '
+                    ] 
+                    }
+
+    for (var numberStr = 0; numberStr < 7; numberStr++) {
+	    str = '';
+        for (var index = 0; index < result.length; index++) {
+            str += dict_ASCII[result[index]][numberStr];
+        }
+        process.stdout.write(str + '\n');
+    }
 
 }
